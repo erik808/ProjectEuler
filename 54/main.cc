c09a895a7430feb5f1c19ec11d2333a927e40772
erik <erik@laptop>
2016-03-01 22:43:08 +0100

    value en score gemaakt, score moet maar al die dingen gaan testen en berekenen tot een unieke score

--- 54/main.cc
+++ 54/main.cc
@@ -7,14 +7,60 @@
 
 typedef std::array<std::array<std::string, 2>, 1000 > Hands;
 void fill_hands(Hands &hands);
+size_t score(std::string hand);
+size_t value(char c);
 
 //------------------------------------------------------------------
 int main()
 {
 	Hands hands;
 	fill_hands(hands);
-	std::cout << hands[0][0] << " " << hands[0][1] << std::endl;
+	std::cout << value('J') << " " << value('4') << '\n';
 	return 0;
+}
+
+//------------------------------------------------------------------
+size_t value(char c)
+{
+	size_t val = 0;
+	switch (c)
+	{
+	case '2': val = 2;
+		break;
+	case '3': val = 3;
+		break;
+	case '4': val = 4;
+		break;
+	case '5': val = 5;
+		break;
+	case '6': val = 6;
+		break;
+	case '7': val = 7;
+		break;
+	case '8': val = 8;
+		break;
+	case '9': val = 9;
+		break;
+	case 'J': val = 11;
+		break;
+	case 'Q': val = 12;
+		break;
+	case 'K': val = 13;
+		break;
+	case 'A': val = 14;
+		break;
+	}
+	return val;
+}
+
+//------------------------------------------------------------------
+size_t score(std::string &hand)
+{
+	size_t score = 0;
+
+	// High card
+	
+	return score;
 }
 
 //------------------------------------------------------------------
